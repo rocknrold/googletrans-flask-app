a746e019fd27b5cccca2d9fc23fb62dbd7b3b180
rocknrold <bsitharold@gmail.com>
2021-01-20 21:17:24 +0800

    initial commit

--- chatup.py
+++ chatup.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
+from googletrans import Translator
+import googletrans
+
 
 # https://flask-socketio.readthedocs.io/en/latest/
 # https://github.com/socketio/socket.io-client
@@ -9,17 +12,28 @@ app = Flask(__name__)
 app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
 socketio = SocketIO( app )
 
+translator = Translator()
+
 @app.route( '/' )
 def hello():
-  return render_template( './ChatApp.html' )
+  return render_template('./ChatApp.html')
+
 
 def messageRecived():
   print( 'message was received!!!' )
 
+# asdsad
 @socketio.on( 'my event' )
 def handle_my_custom_event( json ):
-  print( 'recived my event: ' + str( json ) )
+  if('message' in json):
+    result = translator.translate(json['message'])
+    json['message'] = result.text
+    json['source'] = googletrans.LANGUAGES[result.src.lower()]
+    print( 'recived my event: ' + str( json ) )
   socketio.emit( 'my response', json, callback=messageRecived )
+
 
 if __name__ == '__main__':
   socketio.run( app, debug = True )
+
+
